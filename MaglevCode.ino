d82294c50bb56eb43596bb52d6df81ca8ae9dc25
JoelSmalls <joelokanta46@gmail.com>
2022-01-28 18:07:06 +0000

    <V1.0.1> Adjustment of Code to allow for 2 electromagnets

--- MaglevCode.ino
+++ MaglevCode.ino
@@ -1,10 +1,9 @@
-/* 
-Test Environment of Code, stress testing it and publishing the working versions of the code. 
-Over here, all types of testing will be made to it to ensure success for the project
-*/
-#define PSignal 2 
+#define PSignal 2
 #define IN1 3
 #define IN2 4
+#define PSignal2 5
+#define IN3 6
+#define IN4 7
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -18,27 +17,34 @@ void setup() {
 void loop() {
   // read the input on analog pin 0:
   int sensorValueA = analogRead(A0);
-  //int sensorValueB = analogRead(A1);
+  int sensorValueB = analogRead(A1);
   // print out the value you read:
   Serial.print("SensorA: ");
   Serial.print(sensorValueA);
   Serial.println();
-  //Serial.print("SensorB: ");
-  //Serial.println(sensorValueB);
+  Serial.print("SensorB: ");
+  Serial.println(sensorValueB);
+  Serial.println();
   delay(1);        // delay in between reads for stability
 
-  if (sensorValueA > 600) {
-    // Setting in a range of between 500 and 520 allows for constant spin on each opposite range, but setting it just on 520 makes it react instantly to the magnet's polarity
+  if (sensorValueA < 500) {
+    // Setting in a rangeof between 500 and 520 allows for constant spin on each opposite range, but setting it just on 520 makes it react instantly to the magnet's polarity
     digitalWrite(PSignal, HIGH);
+    digitalWrite(PSignal2, HIGH);
     digitalWrite(IN1, sensorValueA);
     digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
     //Serial.print("Value Greater than 520 for Sensor A");
-    Serial.println();
-  } else if (sensorValueA < 500) {
-    digitalWrite(PSignal, sensorValueA);
+    //Serial.println();
+  } else if (sensorValueB < 500) {
+   digitalWrite(PSignal, HIGH);
+    digitalWrite(PSignal2, HIGH);
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
     //Serial.print("Value Less than 520 for Sensor A");
-    Serial.println();
+    //Serial.println();
   }
 }
